458c4d0d5b6a01fd80221066c87d1bb6ef199494
Raphael Barbo <rsbarbo@gmail.com>
2017-01-31 15:17:38 -0700

    all tests are currently passing

--- spec/features/user_can_visit_home_page_spec.rb
+++ spec/features/user_can_visit_home_page_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe "homepage is complete and setup" do
 
     expect(current_path).to eq root_path
   end
+
+  scenario "expect home page to have content" do
+    visit root_path
+
+    expect(page).to have_content("Welcome to OAuth2 authenticating with social networks")
+  end
+
+  scenario "expect home page to have twitter authentication link" do
+    visit root_path
+
+    expect(page).to have_link("Twitter")
+  end
 end
