1fe3d0bdbe0890b3211df4d6f5244105c81aa9bc
Raphael Barbo <rsbarbo@gmail.com>
2017-01-31 15:40:16 -0700

    update schema with user and update sessions controller to handle user information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id if @user
+      flash[:success] = "Welcome, #{@user.name}!"
+    rescue
+      flash[:warning] = "There was an error while trying to authenticate your account"
+    end
+    redirect_to root_path
   end
 end
