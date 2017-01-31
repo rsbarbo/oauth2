34b0d539f0338250dc8b9d7e594ba78903353d05
Raphael Barbo <rsbarbo@gmail.com>
2017-01-31 14:56:49 -0700

    update routes with auth provider callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  #set root to pages index
   root to: "pages#index"
 
+  #callback sessions setup
+  get '/auth/:provider/callback', to: 'sessions#create'
+  
 end
